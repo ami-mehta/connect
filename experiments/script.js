e54f58f2ad31be08ee1348a6da05283585203831
ami mehta <ajm520@nyu.edu>
2021-03-11 17:36:59 -0500

    more fixes

--- experiments/script.js
+++ experiments/script.js
@@ -2,6 +2,6 @@ let myPara = document.getElementById("my-id-lol");
 console.log(myPara);
 
 function clicked() {
-  myPara.innerHTML = "hahahahahahahahaha lol";
+  myPara.innerHTML = "HA";
   myPara.style.color = "blue";
 }
